bc1f0e3b8c0ad82d2b356a7587d9abe06fd96f27
crazymannick5 <nicolasaaronwhitney1@gmail.com>
2024-02-11 04:53:22 -0500

    Fix some mistakes

--- HWMain.cpp
+++ HWMain.cpp
@@ -2,29 +2,38 @@
 #include <cmath>
 
 int main() {
-
+    double answer = 0;
+    answer = BisectionMain(0,1);
 }
 
-double fFunction(double x){
+/*double fFunction(double x){
     double A = 0;
     double B = 0;
-    B=-1*pow(x,2);
+    B=-1*pow(x,2.0);
     A = exp(B);
+    return(A-pow(x,3.0))
 
+}*/
+
+double fFunction(double x) {
+    return(pow(x,3.0)+x-1)
 }
 
 double BisectionMain(double a1, double b1) {
     double x1 = (a1 + b1)/2.0;
-    fa1 = fFunction(a1);
-    fx1 = fFunction(x1);
-    Mult = fa1 * fx1;
-    if(abs(a1-b1) < .00001) {return((a1+b1)/2)    } else {
+    double fa1 = fFunction(a1);
+    double fx1 = fFunction(x1);
+    double Mult = fa1 * fx1;
+    double ans = 0;
+    
+    if(abs(a1-b1) < .00001) {ans = (a1+b1)/2; std::cout << ans << std::endl;    } else {
         if(Mult < 0) {
-            return(BisectionMain(a1, x1));
+            ans = BisectionMain(a1, x1);
         } else {
-            return(BisectionMain(x1,b1));
+            ans = BisectionMain(x1,b1);
         }
     }
+    return(ans);
 
 }
 
