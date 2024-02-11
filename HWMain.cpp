38c78d8d287ebb6f3b3c6a0cef529d7744156538
crazymannick5 <nicolasaaronwhitney1@gmail.com>
2024-02-11 04:46:34 -0500

    first draft of bM finished

--- HWMain.cpp
+++ HWMain.cpp
@@ -12,3 +12,19 @@ double fFunction(double x){
     A = exp(B);
 
 }
+
+double BisectionMain(double a1, double b1) {
+    double x1 = (a1 + b1)/2.0;
+    fa1 = fFunction(a1);
+    fx1 = fFunction(x1);
+    Mult = fa1 * fx1;
+    if(abs(a1-b1) < .00001) {return((a1+b1)/2)    } else {
+        if(Mult < 0) {
+            return(BisectionMain(a1, x1));
+        } else {
+            return(BisectionMain(x1,b1));
+        }
+    }
+
+}
+
